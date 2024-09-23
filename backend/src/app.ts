b046a1ab5a5836cab8c05fbe5c9274da1713d68f
jonas99Dev <jonas@yohrs.com>
2024-09-23 11:28:48 +0200

    fixat endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,21 @@ import { createServer, IncomingMessage, ServerResponse } from "http";
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = createServer((req: Request, res: Response) => {
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello again World");
-});
 
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Hello 1000 times World");
+  } else if (req.url === "/hello") {
+    res.statusCode = 200;
+    res.end("Hello from the /hello endpoint!");
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+  }
+});
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
